5b6ff12b2504af21547cb01e77ecabfc0168f07f
drizzy009 <yusid03@gmail.com>
2022-06-02 15:44:00 +0100

    newest

--- pages/_app.js
+++ pages/_app.js
@@ -5,14 +5,6 @@ import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
   return (
     <Defaultlayout>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-          integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-          crossorigin="anonymous"
-        ></link>
-      </Head>
       <Component {...pageProps} />
     </Defaultlayout>
   );
